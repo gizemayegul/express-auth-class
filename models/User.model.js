b6b2f82ff9535464287a4bfb194b282ca2d8ff97
gizemayegul <gizemayazyegul@gmail.com>
2023-11-15 14:38:40 +0100

    validation part

--- models/User.model.js
+++ models/User.model.js
@@ -12,6 +12,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: [true, "email is required"],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address :(."],
       unique: true,
       lowercase: true,
       trim: true,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcryptjs = require("bcryptjs");
 const User = require("../models/User.model");
 const saltRounds = 10;
+const mongoose = require("mongoose");
 
 router.get("/signup", (req, res) => {
   res.render("auth/signup");
@@ -11,6 +12,10 @@ router.get("/signup", (req, res) => {
 router.post("/signup", async (req, res) => {
   //console.log("The form data:", req.body);
   const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.render("auth/signup", { errorMessage: "All fields are required" });
+    return;
+  }
   try {
     const getBycrypt = await bcryptjs.genSalt(saltRounds, password);
     const hashPassword = await bcryptjs.hash(getBycrypt, saltRounds);
@@ -21,10 +26,21 @@ router.post("/signup", async (req, res) => {
       password: hashPassword,
     });
     console.log("new created user ", saveDataBase);
-
     res.redirect("/userProfile");
   } catch (error) {
-    console.error(error);
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(500).render("auth/signup", { errorMessage: error.message });
+    } else if (error.code === 11000) {
+      console.log("username and email should be unique");
+    } else {
+      next(error);
+    }
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+      res.status(500).render("auth/signup", {
+        errorMessage: "Password needst ob stronger",
+      });
+    }
   }
 });
 
